5d054ce0174c27f01cacb276d5d095007c505119
sabud <35525196+sabud@users.noreply.github.com>
2018-01-17 15:55:11 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+void main()
+{
+int numb
+printf("Enter an number ");
+scanf("%d",&num);
+if(num%2 == 0)
+printf("%d is an even number", numb;
+else
+printf("%d is an odd number", numb;
+getch();
+}
